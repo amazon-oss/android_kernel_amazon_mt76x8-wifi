bcfde997b4b72b88ba5a229743fa192a8da2f5db
R0rt1z2 <me@r0rt1z2.com>
2026-05-14 09:22:44 +0200

    mt76x8-wifi: Use `TARGET_KERNEL_ARCH` directly
    Looks like in Android 7, KERNEL_ARCH does not get exported at all.
    
    Change-Id: Ib6324e5152f1e7c593ef4f163bef642eab4f363a

--- Android.mk
+++ Android.mk
@@ -16,12 +16,12 @@ _mt76x8_wlan_ko := $(_mt76x8_wlan_intermediates)/$(_mt76x8_wlan_mod_name)$(LOCAL
 KERNEL_OUT := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ
 KERNEL_OUT_RELATIVE := ../../KERNEL_OBJ
 
-$(_mt76x8_wlan_ko): $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/$(BOARD_KERNEL_IMAGE_NAME)
+$(_mt76x8_wlan_ko): $(KERNEL_OUT)/arch/$(TARGET_KERNEL_ARCH)/boot/$(BOARD_KERNEL_IMAGE_NAME)
 	@mkdir -p $(dir $@)
 	@mkdir -p $(KERNEL_MODULES_OUT)/lib/modules
 	@cp -R $(MT76X8_WLAN_PATH)/module/* $(_mt76x8_wlan_intermediates)/
 	@cp -f $(MT76X8_WLAN_PATH)/configs/$(TARGET_DEVICE).config $(_mt76x8_wlan_intermediates)/.config
-	$(hide) +$(KERNEL_MAKE_CMD) $(KERNEL_MAKE_FLAGS) -C $(KERNEL_OUT) M=$(abspath $(_mt76x8_wlan_intermediates)) ARCH=$(KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) MODULE_NAME=$(_mt76x8_wlan_mod_name) modules
+	$(hide) +$(KERNEL_MAKE_CMD) $(KERNEL_MAKE_FLAGS) -C $(KERNEL_OUT) M=$(abspath $(_mt76x8_wlan_intermediates)) ARCH=$(TARGET_KERNEL_ARCH) $(KERNEL_CROSS_COMPILE) MODULE_NAME=$(_mt76x8_wlan_mod_name) modules
 	modules=$$(find $(_mt76x8_wlan_intermediates) -type f -name '*.ko'); \
 	for f in $$modules; do \
 		$(KERNEL_TOOLCHAIN_PATH)strip --strip-unneeded $$f; \
